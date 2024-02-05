118f7bc810e7fb987a258bfface1714329c8d66b
ikhlaasgooly <goolyikhlaas@gmail.com>
2024-02-05 13:02:57 +0400

    comments added

---
+++ Datatypes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int age = 21;
+    double gpa = 3.1;
+    char grade = 'A';
+    char phrase[] = "CURTIN EDUCATION";
+
+/* 1. int (integer): */
+int age = 21;
+
+/* 2. float (single-precision floating-point): */ 
+float height = 5.8;
+
+/* 3. double (double-precision floating-point): */ 
+double gpa = 3.1;
+
+/* 4. char (character):*/ 
+char grade = 'A';
+
+// 5. long (long integer):
+long population = 8000000000L;
+
+// 6. short (short integer):
+short temperature = -10;
+
+// 7. unsigned (unsigned integer):
+unsigned int count = 100;
+
+// 8. _Bool (boolean):
+_Bool isStudent = 1; // 1 represents true, 0 represents false
+
+// 9. void (void type):
+void function() {
+    // Code here
+}
+
+// 10. char array (character array or string):
+char phrase[] = "CURTIN EDUCATION";
+
+
+
+
+    return 0;
+}
