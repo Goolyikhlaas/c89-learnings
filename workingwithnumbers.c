b69917c153d40d52313433ad2472acd76ffe6ed7
ikhlaasgooly <goolyikhlaas@gmail.com>
2024-02-06 13:56:04 +0400

    Working with numbers

---
+++ workingwithnumbers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    
+    /* Print a floating-point number */
+    printf("%f\n\n", 8.9);
+
+    /* Mathematical equations */
+    printf("%s\n", "Mathematical equations");
+
+    /* Addition: Adding 5.0 and 3.5 */
+    printf("%f\n", 5.0 + 3.5); 
+
+    /* Subtraction: Subtracting 3.5 from 5.0 */
+    printf("%f\n", 5.0 - 3.5); 
+
+    /* Division: Dividing 5.0 by 3.5 */
+    printf("%f\n", 5.0 / 3.5); 
+
+    /* Multiplication: Multiplying 5.0 and 3.5 */
+    printf("%f\n\n", 5.0 * 3.5); 
+
+    /* Math functions */
+    printf("%s\n", "Maths functions");
+
+    /* Square root: Calculating the square root of 16 */
+    printf("%f\n", sqrt(16));
+
+    /* Round up: Rounding up the number 1.4 to the nearest integer */
+    printf("%f\n", ceil(1.4));
+
+    /* Round down: Rounding down the number 1.4 to the nearest integer */
+    printf("%f\n", floor(1.4));
+
+    /* Power: Calculating 2 raised to the power of 3 */
+    printf("%f\n", pow(2, 3));
+
+    return 0;
+}
