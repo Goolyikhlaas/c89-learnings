9229f625e6ac2cdd0b9dc810e07dfb5f27e5c903
ikhlaasgooly <goolyikhlaas@gmail.com>
2024-03-10 15:06:22 +0400

    switch_statement

--- switch_statement.c
+++ switch_statement.c
@@ -2,36 +2,37 @@
 #include <stdlib.h>
 
 int main(){
-    char grade = 'A';
+    char grade = 'A'; /* Define a variable 'grade' and initialize it to 'A' */
 
-    switch (grade){
+    switch (grade){ /* Start a switch statement based on the value of 'grade' */
 
-    case 'A':
-        printf("You did Great");
-        break;
+    case 'A': /* If 'grade' is 'A' */
+        printf("You did Great"); /* Print "You did Great" */
+        break; /* Exit the switch statement */
 
-    case 'B':
-        printf("You did Alright");
-        break;
+    case 'B': /* If 'grade' is 'B' */
+        printf("You did Alright"); /* Print "You did Alright" */
+        break; /* Exit the switch statement */
 
-    case 'C':
-        printf("You did Poorly");
-        break;
+    case 'C': /* If 'grade' is 'C' */
+        printf("You did Poorly"); /* Print "You did Poorly" */
+        break; /* Exit the switch statement */
 
-    case 'D':
-        printf("You did very bad");
-        break;
+    case 'D': /* If 'grade' is 'D' */
+        printf("You did very bad"); /* Print "You did very bad" */
+        break; /* Exit the switch statement */
     
-    case 'E':
-        printf("You almost fail");
-        break;  
+    case 'E': /* If 'grade' is 'E' */
+        printf("You almost fail"); /* Print "You almost fail" */
+        break; /* Exit the switch statement */  
 
-    case 'F':
-        printf("You Failed");
-        break;
-    default :
-        printf("No result");
+    case 'F': /* If 'grade' is 'F' */
+        printf("You Failed"); /* Print "You Failed" */
+        break; /* Exit the switch statement */
+
+    default : /* If 'grade' is none of the above */
+        printf("No result"); /* Print "No result" */
     }
 
-    return 0;
+    return 0; /* Exit the program */
 }
